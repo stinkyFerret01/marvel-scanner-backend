f99d85414c6ce6d243d264d9afa24bd1d6bee651
Lafon Christophe <lfnchrstph@mbp-de-lafon.lan>
2022-08-03 14:31:57 +0200

    cors update

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 ///ROUTES////
