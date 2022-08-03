e35f79376e8997556f5c40fe506d42a4938c3f60
Lafon Christophe <lfnchrstph@mbp-de-lafon.lan>
2022-08-03 19:15:58 +0200

    comics Route

--- index.js
+++ index.js
@@ -45,6 +45,24 @@ app.get("/character", (req, res) => {
   }
 });
 
+app.get("/comics", (req, res) => {
+  try {
+    axios
+      .get(
+        `https://lereacteur-marvel-api.herokuapp.com/character/${req.query.id}?apiKey=${process.env.API_KEY}`
+      )
+      .then((response) => {
+        let charcomicsData = response.data;
+        res.json(charcomicsData);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+});
+
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server has Started!!");
 });
