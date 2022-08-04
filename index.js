7d4e6bd576ef13e5d5103c91c346ef6866926837
Lafon Christophe <lfnchrstph@mbp-de-lafon.lan>
2022-08-04 12:55:53 +0200

    search by name

--- index.js
+++ index.js
@@ -11,17 +11,31 @@ app.use(express.json());
 
 app.get("/characters", (req, res) => {
   try {
-    axios
-      .get(
-        `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}`
-      )
-      .then((response) => {
-        let goodData = response.data.results;
-        res.json(goodData);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    if (req.query.name) {
+      axios
+        .get(
+          `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}`
+        )
+        .then((response) => {
+          let goodData = response.data.results;
+          res.json(goodData);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      axios
+        .get(
+          `https://lereacteur-marvel-api.herokuapp.com/characters?name=${req.query.name}apiKey=${process.env.API_KEY}`
+        )
+        .then((response) => {
+          let goodData = response.data.results;
+          res.json(goodData);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
   } catch (error) {
     res.status(400).json(error.message);
   }
